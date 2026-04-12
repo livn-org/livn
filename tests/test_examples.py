c5162a83b07e15b11d59887c90362d0d8150cde1
Frithjof Gressmann <frithjof@noc.io>
2026-04-12 01:11:17 -0500

    Skip

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -5,6 +5,8 @@ import pytest
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
+ON_CI = os.getenv("CI") == "true"
+
 
 def test_using_the_dataset():
     pytest.importorskip("datasets")
@@ -14,6 +16,7 @@ def test_using_the_dataset():
 @pytest.mark.skipif(
     not os.getenv("LIVN_BACKEND"), reason="no simulation backend selected"
 )
+@pytest.mark.skipif(ON_CI, reason="predefined system not available on CI")
 def test_run_a_simulation():
     from examples import run_a_simulation
 
@@ -23,6 +26,7 @@ def test_run_a_simulation():
 @pytest.mark.skipif(
     not os.getenv("LIVN_BACKEND"), reason="no simulation backend selected"
 )
+@pytest.mark.skipif(ON_CI, reason="predefined system not available on CI")
 def test_parallel_simulation():
     pytest.importorskip("matplotlib")
     from examples import parallel_simulation
@@ -31,5 +35,6 @@ def test_parallel_simulation():
 
 
 @pytest.mark.skipif(os.getenv("LIVN_BACKEND") != "diffrax", reason="diffrax only")
+@pytest.mark.skipif(ON_CI, reason="predefined system not available on CI")
 def test_differentiable_simulation():
     from examples import differentiable_simulation  # noqa: F401
